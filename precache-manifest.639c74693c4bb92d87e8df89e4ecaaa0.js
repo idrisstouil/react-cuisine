440a5b4207031d8c11d7b81881da6f8b4b48e114
TOUIL Idriss <idrisstouil44@gmail.com>
2020-04-26 03:14:41 +0000

    Updates

---
+++ precache-manifest.639c74693c4bb92d87e8df89e4ecaaa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9305aecf4fa67a278bede1ba558d08",
+    "url": "/react-cuisine/index.html"
+  },
+  {
+    "revision": "4358f0a55e677954f420",
+    "url": "/react-cuisine/static/css/main.fab2557a.chunk.css"
+  },
+  {
+    "revision": "4841ea17566a6c43ce5a",
+    "url": "/react-cuisine/static/js/2.c8392f0e.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/react-cuisine/static/js/2.c8392f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4358f0a55e677954f420",
+    "url": "/react-cuisine/static/js/main.0781fe89.chunk.js"
+  },
+  {
+    "revision": "ff5cf28805f0300b695f",
+    "url": "/react-cuisine/static/js/runtime-main.cc90359b.js"
+  }
+]);
